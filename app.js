b10b12f597ddd4793926d6ec70986d2112384eaf
kaligiménez <clagsuso@gmail.com>
2021-07-10 15:07:25 -0300

    Pase 2

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req,res) =>{
-   res.sendFile(path.join(__dirname, './views/home.html')); 
+   res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'register')); 
+});
+
+
+app.use(express.static(path.join(__dirname, './public')));  
+
+app.use(express.static(path.join(__dirname, './views')));
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor mercadoliebre corriendo");
+    console.log("Servidor corriendo");
 });
