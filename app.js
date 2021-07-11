1ff8faa8816ccaceb9221d1326ee8bb058df83dc
kaligiménez <clagsuso@gmail.com>
2021-07-11 20:39:16 -0300

    anteultima subida

--- app.js
+++ app.js
@@ -12,10 +12,23 @@ app.get('/register', (req,res) =>{
 });
 
 
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + 'register');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + 'login');
+});
+
+
 app.use(express.static(path.join(__dirname, './public')));  
 
 app.use(express.static(path.join(__dirname, './views')));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
-});
+})
